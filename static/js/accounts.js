633cd2115bda4ff53cb8176885aa72e7d5964076
yillkid <yillkid@gmail.com>
2023-07-09 18:30:19 +0800

    fix: 前端圖片上傳的非同步特效

--- static/js/accounts.js
+++ static/js/accounts.js
@@ -38,8 +38,8 @@ function signup(dataJSON) {
   return resultJSON;
 }
 
-function change_pw(dataJSON) {     
-  var resultJSON = {};   
+function change_pw(dataJSON) {
+  var resultJSON = {};
   $.ajax({
     url: HOST_URL_EID_DAEMON + "/accounts/modify",
     type: "POST",
@@ -55,33 +55,6 @@ function change_pw(dataJSON) {
   });
 }
 
-function uploadAvatarImg(base64Img) {
-  var dataJSON = {};
-  var resultBool = false;
-  dataJSON.email = getLocalStorage("email");
-  dataJSON.img = base64Img;
-  $.ajax({
-    url: HOST_URL_EID_DAEMON + "/accounts/modify",
-    type: "POST",
-    async: false,
-    crossDomain: true,
-    data:  dataJSON,
-    success: function(returnData) {
-      const obj = JSON.parse(returnData);
-      if (obj.result) {
-        resultBool = true;
-      } else {
-        resultBool = false;
-      }
-    },
-    error: function(xhr, ajaxOptions, thrownError){
-      console.log(thrownError);
-      resultBool = false;
-    }
-  });
-
-  return resultBool;
-}
 
 function modify_username(username) {
   // Modify account
@@ -110,7 +83,7 @@ function getAccountAvatarImg() {
   var dataJSON = {};
   var resultJSON = {};
   dataJSON.email = getLocalStorage("email");
-  
+
   $.ajax({
     url: HOST_URL_EID_DAEMON + "/accounts/get_avatar_img",
     type: "POST",
